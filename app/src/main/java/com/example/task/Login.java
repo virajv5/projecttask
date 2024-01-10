68e29ed6f3d3b6a1f0953809b0470803b00f44df
Virajv5 <virajgajdharv5@gmail.com>
2024-01-10 22:48:44 +0530

    Authentication with google fire base. This does not support custom input. It will be possible to enter custom id pass in next code.

---
+++ app/src/main/java/com/example/task/Login.java
@@ -0,0 +1,144 @@
+package com.example.task;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Dialog;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.view.Window;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.snackbar.Snackbar;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.GoogleAuthProvider;
+
+public class Login extends AppCompatActivity {
+
+
+    private static final String TAG = "GOOGLEAUTH";
+
+    GoogleSignInClient mgoogleSignInClient;
+
+    private FirebaseAuth mAuth;
+
+    Button signin;
+
+    EditText uname,pass;
+    TextView forgetpass;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        mAuth = FirebaseAuth.getInstance();
+
+        //configuring Google Sign in with requested email
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.default_web_client_id))
+                .requestEmail()
+                .build();
+        mgoogleSignInClient = GoogleSignIn.getClient(this, gso);
+        signin = findViewById(R.id.google);
+        uname = findViewById(R.id.username);
+        pass = findViewById(R.id.password);
+        forgetpass = findViewById(R.id.forget);
+
+        signin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                signIn();
+            }
+        });
+
+
+    }
+//
+//    void signIn() {
+//        // initiates google sign in process
+//        mgoogleSignInClient.revokeAccess();
+//        Intent intent = mgoogleSignInClient.getSignInIntent();
+//        startActivityForResult(intent, 1000);
+//    }
+
+
+    void signIn() {
+        // Revoke access before initiating google sign in process
+        mgoogleSignInClient.revokeAccess().addOnCompleteListener(this, new OnCompleteListener<Void>() {
+            @Override
+            public void onComplete(@NonNull Task<Void> task) {
+                // Now, initiate google sign in process
+                Intent intent = mgoogleSignInClient.getSignInIntent();
+                startActivityForResult(intent, 1000);
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (data != null) {
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+            try {
+                GoogleSignInAccount account = task.getResult(ApiException.class);
+                firebaseAuthwithGoogle(account.getIdToken());
+            } catch (ApiException e) {
+                Log.w(TAG, "Google Sign In Failed", e);
+            }
+        }
+
+        if (requestCode == 1000) {
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+            try {
+                GoogleSignInAccount account = task.getResult(ApiException.class);
+
+
+                firebaseAuthwithGoogle(account.getIdToken());
+            } catch (ApiException e) {
+                Log.w(TAG, "Google Sign In Failed", e);
+            }
+        }
+    }
+
+    private void firebaseAuthwithGoogle(String idToken) {
+        AuthCredential credential = GoogleAuthProvider.getCredential(idToken, null);
+        mAuth.signInWithCredential(credential)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            Log.d(TAG, "SignInWithCredentials:success");
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            Intent i = new Intent(Login.this, Logout.class);
+                            startActivity(i);
+                            finish();
+                            Toast.makeText(Login.this, "Success", Toast.LENGTH_SHORT).show();
+
+                        } else {
+
+                        }
+                    }
+                });
+    }
+}
+
+
+
--- app/src/main/java/com/example/task/Logout.java
+++ app/src/main/java/com/example/task/Logout.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
 
@@ -24,10 +25,18 @@ public class Logout extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 mauth.signOut();
-                Intent i=new Intent(Logout.this, MainActivity.class);
+                Intent i=new Intent(Logout.this, Login.class);
+
+               //flags added so that if we click back button on the login page it won't show up the logout page.
+                i.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK
+                        | Intent.FLAG_ACTIVITY_CLEAR_TOP
+                        | Intent.FLAG_ACTIVITY_CLEAR_TASK);
                 startActivity(i);
+
+                Toast.makeText(Logout.this, "LogOut", Toast.LENGTH_SHORT).show();
                 finish();
             }
         });
     }
+
 }
